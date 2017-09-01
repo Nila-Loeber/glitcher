845afe1e23197814e12b9bf00b5fd458fcea04fc
NilsL <nils@Nilss-MBP.fritz.box>
2017-09-01 16:43:52 +0200

    sd

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-var http = require("http");
+var http = require('http');
 
-http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(process.env.glitcher_httpport);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
